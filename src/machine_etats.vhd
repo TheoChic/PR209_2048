2334ecabf3be59f03fb13ad5ccc98fa853471134
mlebat <78764534+mlebat@users.noreply.github.com>
2021-03-24 10:40:27 +0100

    update machine_etats
    /

--- src/machine_etats.vhd
+++ src/machine_etats.vhd
@@ -8,20 +8,28 @@ entity machine_etats is
 	       
 	       btn_haut, btn_bas, btn_droit, btn_gauche    : IN STD_LOGIC;
 	       
-	       index_grille                                : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-	       cpt_init                                    : OUT STD_LOGIC;	              
-           
-           value                                       : OUT STD_LOGIC_VECTOR(255 DOWNTO 0));
+	       cmp, zero, en_dec                           : IN STD_LOGIC;
+	       
+	       dec                                         : IN STD_LOGIC_VECTOR(12 DOWNTO 0);	
+	       
+	       index_read, index_write                     : IN STD_LOGIC_VECTOR(3 DOWNTO 0);
+	       cpt_write_en, cpt_read_en1, cpt_read_en2    : OUT STD_LOGIC;
+	       
+	       read_en, write_en                           : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
+	       write_data                                  : OUT STD_LOGIC_VECTOR(11 DOWNTO 0));
+	       
            -- index : 4 bits de poids fort : index grille iijj
            -- puissance :  12 bits de poids faible : valeur de la puissance de 2
            -- grille de 16 cases : 16 vecteurs de taille 11 DONWTO 0 -> 192
            
-           TYPE Etat is (Init, Ajout_nombre, Export_grille, Waiting, Decalage, Calcul);
+           TYPE Etat is (Init, Ajout_nombre, Export_grille, Waiting, Recherche, Decalage, Ajout_zero, Calcul);
            SIGNAL pr_state, nx_state : Etat := Init;
            
 end machine_etats;
 
 architecture beh_machine_etats of machine_etats is
+
+signal z    : std_logic;
 
 SIGNAL val_btn      : STD_LOGIC_VECTOR(1 DOWNTO 0);
 signal cal_next     : std_logic;
@@ -35,8 +43,6 @@ SIGNAL index_j      : STD_LOGIC_VECTOR(1 DOWNTO 0);
 SIGNAL add          : integer;
                                                     
 begin
-    index_j <= index_grille(3 DOWNTO 2);
-    index_i <= index_grille(1 DOWNTO 0);
 
 maj_etat : PROCESS ( CLK , RST )
     BEGIN
@@ -49,41 +55,21 @@ END PROCESS maj_etat;
 
 cal_nx_state : PROCESS (RST, pr_state)
     BEGIN
-    
         CASE pr_state IS
         
-            WHEN Init =>    
-                cpt_init <= '1';
-                IF (RST = '0') THEN
-                    grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= "000000000000";
-                    
-                    if(to_integer(unsigned(index_i)) = 3 and to_integer(unsigned(index_i)) = 3) then
-                        nx_state <= Ajout_nombre;
-                    end if;
-                    ELSE
-                        nx_state <= Init;
-                    END IF;
+            WHEN Init => 
+                -- TO DO  
+                nx_state <= Ajout_nombre;                                 
 
             WHEN Ajout_nombre =>
-                cpt_init <= '1';
-                
-                if(grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) = "000000000000") then
-                    grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= "000000000010";  
-                    nx_state <= Export_grille;
-                end if;
+                -- TO DO  
+                nx_state <= Export_grille;
               
             WHEN Export_grille => 
-                cpt_init <= '1';
-                value(255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) DOWNTO 255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) - 1 ) <= index_j;
-                value(255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) - 2 DOWNTO 255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) - 3 ) <= index_i;
-                value(255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) - 4 DOWNTO 255 - 16 * to_integer(unsigned(index_i)) - 4 * 16 * to_integer(unsigned(index_j)) - 11 - 4 ) <= grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 );
-                
-                if(to_integer(unsigned(index_i)) = 3 and to_integer(unsigned(index_j)) = 3) then
-                    nx_state <= Waiting;
-                end if;
+                -- TO DO
+                nx_state <= Waiting;
             
-            WHEN Waiting =>
-                
+            WHEN Waiting =>                
                 cal_next <= '0';
                 
                 if (btn_haut = '1') then 
@@ -107,44 +93,25 @@ cal_nx_state : PROCESS (RST, pr_state)
                     cal_next <= '0';
                     nx_state <= Decalage;
                 end if;
-           
+                
+            WHEN Recherche =>
+                       
+                if(zero = '1') then
+                    nx_state <= Decalage;              
+                elsif(zero = '0') then
+                    nx_state <= Export_grille;
+                end if;
+                
             WHEN Decalage =>
-                
-                cpt_init <= '1';         
-                
                 case val_btn IS
                     when "00" => -- incrémentation i, incrémentation j
-                        if(grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) = "000000000000") then
-                            if(to_integer(unsigned(index_j)) < 3) then
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) + 1) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) + 1) - 11 );
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) + 1) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) + 1) - 11 ) <= "000000000000";
-                            end if;
-                        end if;
 
                     when "01" => -- décrémentation i, décrémentation j
-                        if(grille(191 - 12 * to_integer(3 - unsigned(index_i)) - 4 * 12 * to_integer(3 - unsigned(index_j)) DOWNTO 191 - 12 * to_integer(3 - unsigned(index_i)) - 4 * 12 * to_integer(3 - unsigned(index_j)) - 11 ) = "000000000000") then
-                            if(to_integer(3 - unsigned(index_i)) > 0) then
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= grille(191 - 12 * to_integer(unsigned(index_i) - 1) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i) - 1) - 4 * 12 * to_integer(unsigned(index_j)) - 11 );
-                                grille(191 - 12 * to_integer(unsigned(index_i) - 1) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i) - 1) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= "000000000000";
-                            end if;
-                        end if;
-       
+    
                     when "10" => -- décrémentation i, décrémentation j
-                        if(grille(191 - 12 * to_integer(3 - unsigned(index_i)) - 4 * 12 * to_integer(3 - unsigned(index_j)) DOWNTO 191 - 12 * to_integer(3 - unsigned(index_i)) - 4 * 12 * to_integer(3 - unsigned(index_j)) - 11 ) = "000000000000") then
-                            if(to_integer(3 - unsigned(index_j)) > 0) then
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) - 1) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) - 1) - 11 );
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j) - 1) DOWNTO 191 - 12 * to_integer(unsigned(index_i) - 1) - 4 * 12 * to_integer(unsigned(index_j) - 1) - 11 ) <= "000000000000";
-                            end if;
-                        end if;
-                            
+
                     when "11" => -- incrémentation i, incrémentation j
-                        if(grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) = "000000000000") then
-                            if(to_integer(unsigned(index_j)) < 3) then
-                                grille(191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i)) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= grille(191 - 12 * to_integer(unsigned(index_i) + 1) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i) + 1) - 4 * 12 * to_integer(unsigned(index_j)) - 11 );
-                                grille(191 - 12 * to_integer(unsigned(index_i) + 1) - 4 * 12 * to_integer(unsigned(index_j)) DOWNTO 191 - 12 * to_integer(unsigned(index_i) + 1) - 4 * 12 * to_integer(unsigned(index_j)) - 11 ) <= "000000000000"; 
-                            end if;
-                        end if;
-                    
+
                     when others => -- do nothing
                                                                             
                 end case;
@@ -153,9 +120,12 @@ cal_nx_state : PROCESS (RST, pr_state)
                     nx_state <= Calcul;
                 end if;
                 
+                
+            WHEN Ajout_zero =>
+                -- TO DO  
+                nx_state <= Export_grille;
+                
             when Calcul =>  
-            
-                cpt_init <= '1';
 
                     case val_btn IS
                         when "00" =>
