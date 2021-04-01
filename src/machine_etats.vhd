8492735d4a721d75358ad1bee83de710e2b0e9cf
Théo <theochicard@gmail.com>
2021-04-01 07:40:46 +0200

    tb_fsm fonctionnel

--- src/machine_etats.vhd
+++ src/machine_etats.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL ;
 
-
+-- TODO : CHECK EN FIN De cycle, quand on reste dans recherche 0 même après un addr max, avant le sig on doit faire un décalage, et après on doit aller dans ajour nombre : TB 96us
 entity machine_etats is
   Port (    CLK, RST, CE            : in STD_LOGIC;
                        
@@ -10,6 +10,7 @@ entity machine_etats is
             cmp, zero               : in std_logic;
             
             btn                     : in STD_LOGIC;
+            init_btn                : out STD_LOGIC;
           
             cpt_ram_grille_en, cpt_ram_grille_init        : out STD_LOGIC;
             addr_max_ram_grille     : in STD_LOGIC;
@@ -26,22 +27,29 @@ end machine_etats;
 
 architecture beh_machine_etats of machine_etats is
 
-type Etat is (INIT, AJOUT_NOMBRE, EXPORT_GRILLE, WAITING, RECHERCHE_ZERO, DECALAGE, AJOUT_ZERO, SAVING, CALCUL);
+type Etat is (INIT,INIT_GRILLE, AJOUT_NOMBRE, RESET_ADDR_GRILLE, EXPORT_GRILLE, WAITING, RECHERCHE_ZERO, DECALAGE, AJOUT_ZERO, SAVING, CALCUL, ADD);
 signal pr_state, nx_state : Etat := Init;
 
-signal cpt : integer range 1 to 5 := 0; 
+signal cpt : integer range 1 to 5 := 1; 
+signal init_intern_cpt_1, init_intern_cpt_2 : STD_LOGIC;
 signal enable_cpt_intern : STD_LOGIC;
 
-signal bool_calc : boolean := FALSE;
+signal bool_calc, bool_ajout_nombre  : boolean := FALSE;
 signal previous_state : Etat := INIT;
                                                
 begin
     cpt_case : process( clk, rst)
         begin
             if ( rst = '0') then
-                cpt <= 0 ;
-            elsif ( clk'event and clk = '1' and CE = '1' and enable_cpt_intern = '1') then
-                cpt <= cpt + 1 ;
+                cpt <= 1 ;
+            elsif ( clk'event and clk = '1' and CE = '1') then
+                if(init_intern_cpt_1 = '1') then 
+                    cpt <= 1;
+                elsif (init_intern_cpt_2 = '1') then
+                    cpt <= 2;
+                elsif enable_cpt_intern = '1' then 
+                    cpt <= cpt + 1; 
+                end if;
             end if;   
     end process cpt_case;
      
@@ -54,17 +62,98 @@ begin
             end if;     
     end process maj_etat;
 
-    cal_sig : process (rst, pr_state) --process transition
+    cal_sig : process (pr_state, addr_max_ram_grille, bool_calc, zero, previous_state, cpt, cmp) --process action
         begin
         
             case pr_state is
             
-                when INIT =>    
+                when INIT => 
+                    -- on balance la grille de 0
+                    sel_dataw           <= "00";
+                    sel_addw            <= '0';
+                    cpt_ram_grille_en   <= '0';
+                    w_en_ram_grille     <= '0';
+
+                    enable_cpt_intern   <= '0';
+                    sel_addr            <= '0'; 
+                    w_en_ram_ext        <= '0';
+                    cpt_ram_ext_en      <= '0';
+                    cpt_ram_ext_init    <= '1';
+                    cpt_ram_grille_init  <= '1';
+
+                    init_btn <= '0';
+                    
+                    init_intern_cpt_1 <= '0';
+                    init_intern_cpt_2 <= '0';
+                    
+                    previous_state <= INIT;
+                    bool_ajout_nombre <= TRUE;
+                    
+                when INIT_GRILLE => 
+                    -- on balance la grille de 0
+                    sel_dataw           <= "00";
+                    sel_addw            <= '0';
+                    cpt_ram_grille_en   <= '1';
+                    w_en_ram_grille     <= '1';
+                  
+                    cpt_ram_grille_init <= '0';
+                   
+                    
+                    enable_cpt_intern   <= '0';
+                    sel_addr            <= '0'; 
+                    w_en_ram_ext        <= '0';
+                    cpt_ram_ext_en      <= '0';
+                    cpt_ram_ext_init    <= '0';
+                    cpt_ram_grille_init  <= '0';
+
+                    init_btn <= '0';
+                    
+                    init_intern_cpt_1 <= '0';
+                    init_intern_cpt_2 <= '0';
+                    
+                when RESET_ADDR_GRILLE =>
+                    sel_dataw           <= "00";
+                    sel_addw            <= '0';
+                    cpt_ram_grille_en   <= '0';
+                    w_en_ram_grille     <= '0';
+
+                    enable_cpt_intern   <= '0';
+                    sel_addr            <= '0'; 
+                    w_en_ram_ext        <= '0';
+                    cpt_ram_ext_en      <= '0';
+                    cpt_ram_ext_init    <= '0';
+                    cpt_ram_grille_init  <= '1';
+
+                    init_btn <= '0';
+                    
+                    init_intern_cpt_1 <= '1';
+                    init_intern_cpt_2 <= '0';
+                    
+                    previous_state <= RESET_ADDR_GRILLE;
+                    
+                    cpt_ram_grille_init <= '1';
                     
                 when AJOUT_NOMBRE =>
                     -- on balance la grille de 0
-                    cpt_ram_ext_init    <= '1';
+                    
+                    sel_dataw           <= "11";
+                    sel_addw            <= '0';
+                    w_en_ram_grille     <= '1';
+                    
+                    cpt_ram_grille_en   <= '0';
+                    cpt_ram_grille_init <= '0';                  
+                    enable_cpt_intern   <= '0';
+                    sel_addr            <= '0'; 
+                    w_en_ram_ext        <= '0';
+                    cpt_ram_ext_en      <= '0';
+                    cpt_ram_ext_init    <= '0';
+                    init_btn <= '0';
+                    init_intern_cpt_1 <= '0';
+                    init_intern_cpt_2 <= '0';
+                    
+                    bool_ajout_nombre <= FALSE;
 
+                    
                 when EXPORT_GRILLE => 
                     w_en_ram_ext        <= '1';
                     cpt_ram_ext_en      <= '1';
@@ -78,10 +167,14 @@ begin
                     sel_addr        <= '0'; 
                     sel_addw            <= '0';
     
-
+                    init_btn <= '1';
+                    init_intern_cpt_1 <= '0';
+                    init_intern_cpt_2 <= '0';
                     --activer un compteur qui ballaye toutes les cases, on reste dans l'état tant que pas addr max
                     --activer l'écriture sur la ram CPU qui fonctionnera en front descendant
                     --correctement orienter les mux    
+                    
+
                 when WAITING =>
                     w_en_ram_ext        <= '0';
                     cpt_ram_ext_en      <= '0';
@@ -93,37 +186,56 @@ begin
                     w_en_ram_grille <= '0';
                     sel_dataw       <= "00";
                     sel_addr        <= '0';  
+                    init_btn <= '0';
                     
-                    cpt <= 0;
+                    bool_calc <= FALSE;
+                    init_intern_cpt_1 <= '1';
+                    init_intern_cpt_2 <= '0';
+                    previous_state <= WAITING;
+                    
                 when RECHERCHE_ZERO =>
-                    if cpt < 4 then 
-                        if zero = '1' then
-                            cpt_ram_grille_en   <= '0';
-                            enable_cpt_intern   <= '0';
-                        else
-                            cpt_ram_grille_en   <= '1';
-                            enable_cpt_intern   <= '1';
-                        end if;
+                    if cpt > 3 then  --Le CPT interne permet d'ignorer la dernière valeur d'une ligne
+                        init_intern_cpt_1   <= '1';
                     else
-                        cpt_ram_grille_en   <= '1';
-                        enable_cpt_intern   <= '0';
-                        cpt <= 1;
+                        init_intern_cpt_1   <= '0';
                     end if;
                     
-                    if addr_max_ram_grille = '1' then
-                        cpt_ram_grille_init  <= '1';
-                    end if;
+                    cpt_ram_grille_en   <= '1';
+                    enable_cpt_intern   <= '1';
+                    
+                    if addr_max_ram_grille = '1' and bool_calc = TRUE then
+                        bool_ajout_nombre <= TRUE; 
+                    end if;       
+                          
+                    sel_addr            <= '0';  
+
+                    init_intern_cpt_2 <= '0';
+                    w_en_ram_ext        <= '0';
+                    cpt_ram_ext_en      <= '0';
+                    cpt_ram_ext_init    <= '0';
+                    cpt_ram_grille_init    <= '0';
+
+                    sel_addw            <= '0';
+                    w_en_ram_grille     <= '0';
+                    sel_dataw           <= "00";
+                    init_btn <= '0';
                     
                 when DECALAGE =>
                     w_en_ram_grille <= '1';
                     sel_dataw       <= "01";
-                    sel_addr        <= '1';
+                    sel_addw        <= '1';
+                    sel_addr        <= '0';
                     
-                    sel_addw            <= '0';
+                 
+                    
                     cpt_ram_grille_en   <= '0';
                     enable_cpt_intern   <= '0';
                     cpt_ram_grille_init <= '0';
                     previous_state      <= DECALAGE;
+                    init_btn <= '0';
+                    
+                    init_intern_cpt_1   <= '0';
+                    init_intern_cpt_2   <= '0';
                     
                 when AJOUT_ZERO =>
                     w_en_ram_grille    <= '1';
@@ -131,70 +243,115 @@ begin
                     
                     sel_addr            <= '0';
                     sel_addw            <= '0';
-                    cpt_ram_grille_en   <= '1'; --FAIRE ATTENTION A CE QUE CE SOIT ACTIF SUR LE FRONT D'APRES : si je l'active maintenant, c'est pour qu'au coup d'H suivant on puisse incrémenter
-                    enable_cpt_intern   <= '1';
+                    
+                    
+                    cpt_ram_grille_en   <= '0'; --FAIRE ATTENTION A CE QUE CE SOIT ACTIF SUR LE FRONT D'APRES : si je l'active maintenant, c'est pour qu'au coup d'H suivant on puisse incrémenter
+                    enable_cpt_intern   <= '0';
+                    
+                    
+                    --previous_state <= AJOUT_ZERO;
+                    init_btn <= '0';
+                    init_intern_cpt_1   <= '0';
+                    init_intern_cpt_2   <= '0';
                     
                 when SAVING =>
                     cpt_ram_grille_en   <= '1'; 
                     enable_cpt_intern   <= '0';
-                    cpt                 <= 2;    
-                                    
+                    init_intern_cpt_2   <= '1';
+                    
+                    init_intern_cpt_1   <= '0';                                    
                     w_en_ram_grille     <= '0';
                     sel_dataw       <= "00";
                     sel_addr        <= '0';
                     sel_addw        <= '0';
                     cpt_ram_grille_init   <= '0';     
-                               
+                    --previous_state <= SAVING; 
+                    init_btn <= '0';    
+                          
                 when CALCUL =>  
+                                        
                     bool_calc <= TRUE;
-                    
-                    if cmp = '1' then
-                        w_en_ram_grille            <= '1';
-                    else
-                        w_en_ram_grille            <= '0';
-                    end if;   
-                    
-                    if addr_max_ram_grille = '1' then
-                        cpt_ram_grille_init  <= '1';
-                    end if;
-                
-                    sel_dataw           <= "10";
-                    sel_addw            <= '0';
+                                    
                     cpt_ram_grille_en   <= '1'; 
                     enable_cpt_intern   <= '1';
-                                    
+                    
+                    
+                    w_en_ram_grille     <= '0';
+                    sel_dataw           <= "00";
+                    sel_addw            <= '0';
                     sel_addr             <= '0';
                     cpt_ram_grille_init  <= '0';        
-                    previous_state <= CALCUL;
+
+                    init_btn <= '0';
+                    init_intern_cpt_1   <= '0';
+                    init_intern_cpt_2   <= '0';
+                    
+                when ADD =>  
+                                                                            
+                    sel_dataw           <= "10";
+                    sel_addw            <= '1';
+                    w_en_ram_grille     <= '1';
+
+                    cpt_ram_grille_en   <= '0'; 
+                    enable_cpt_intern   <= '0';
+                    
+                    sel_addr             <= '0';
+                    cpt_ram_grille_init  <= '0';        
+                    previous_state  <= ADD;
+                    init_btn <= '0';
+                    init_intern_cpt_1   <= '0';
+                    init_intern_cpt_2   <= '0';
+                    
+                    
                 end case;
             
     end process cal_sig;
 
-    cal_nx_state : process (rst, pr_state) --process signaux
+    cal_nx_state : process (rst, pr_state, addr_max_ram_grille, btn, bool_calc, zero, previous_state, cpt, cmp) --process transition
         begin
         
             case pr_state is
             
                 when INIT =>    
-                    -- on balance la grille de 0
+                    nx_state <= INIT_GRILLE;
+                    
+                when INIT_GRILLE =>    
+                -- on balance la grille de 0
+                if (addr_max_ram_grille = '1') then
+                    nx_state <= RESET_ADDR_GRILLE;
+                end if;
+                
+                when RESET_ADDR_GRILLE =>    
+                    nx_state <= RECHERCHE_ZERO;
+                    
                 when AJOUT_NOMBRE =>
+                     nx_state <= EXPORT_GRILLE;
+
                     --pas de random, cpt qui balmlait les case et met un 0 qq part    
                 when EXPORT_GRILLE => 
+                    if addr_max_ram_grille = '1' then
+                        nx_state <= WAITING;
+                    end if;
+                    
                     --activer un compteur qui ballaye toutes les cases, on reste dans l'état tant que pas addr max
                     --activer l'écriture sur la ram CPU qui fonctionnera en front descendant
                     --correctement orienter les mux 
                 when WAITING =>
+                    if btn = '1' then
+                        nx_state <= RECHERCHE_ZERO;
+                    end if;
                     --tant que pas de bouton, on reste
                 when RECHERCHE_ZERO =>
+                    
                     if addr_max_ram_grille = '1' and bool_calc = FALSE then
                         nx_state <= SAVING;
-                        
-                    elsif addr_max_ram_grille = '1' and bool_calc = TRUE then
+                    
+                    elsif (zero = '1' and bool_ajout_nombre = TRUE) then
                         nx_state <= AJOUT_NOMBRE;
                         
-                    elsif zero = '1' then
-                        nx_state <= DECALAGE;
-                    
+                    elsif (zero = '1' and cpt <= 3) then
+                        nx_state <= DECALAGE;   
+                        
                     else
                         nx_state <= RECHERCHE_ZERO;
                     end if;
@@ -205,7 +362,7 @@ begin
                 when AJOUT_ZERO =>
                     if previous_state = DECALAGE then
                         nx_state <= RECHERCHE_ZERO;
-                    elsif previous_state = CALCUL then
+                    elsif previous_state = ADD then
                         nx_state <= CALCUL;
                     else 
                         nx_state <= AJOUT_ZERO;
@@ -216,20 +373,22 @@ begin
                     
                 when CALCUL =>  
                     if addr_max_ram_grille = '1' then
-                        nx_state <= RECHERCHE_ZERO;
+                        nx_state <= RESET_ADDR_GRILLE;
                         
-                    elsif cpt <= 4 then
+                    elsif cpt >= 4 then
                         nx_state <= SAVING;
                         
                     elsif cmp = '1' then
-                        nx_state <= AJOUT_ZERO;
+                        nx_state <= ADD;
                     
                     else
                         nx_state <= CALCUL;
                     end if;
                     
+                when ADD =>
+                    nx_state <= AJOUT_ZERO;
+                        
                 end case;
             
     end process cal_nx_state;
-
 end beh_machine_etats;
--- tb/tb_machine_etats.vhd
+++ tb/tb_machine_etats.vhd
@@ -1,6 +1,6 @@
 -- Testbench automatically generated online
 -- at https://vhdl.lapinoo.net
--- Generation date : 15.3.2021 14:09:34 UTC
+-- Generation date : 31.3.2021 12:44:57 UTC
 
 library ieee;
 use ieee.std_logic_1164.all;
@@ -11,28 +11,48 @@ end tb_machine_etats;
 architecture tb of tb_machine_etats is
 
     component machine_etats
-        port (CLK          : in std_logic;
-              RST          : in std_logic;
-              CE           : in std_logic;
-              btn_haut     : in std_logic;
-              btn_bas      : in std_logic;
-              btn_droit    : in std_logic;
-              btn_gauche   : in std_logic;
-              index_grille : in std_logic_vector (3 downto 0);
-              cpt_init     : out std_logic;
-              value        : out std_logic_vector (255 downto 0));
+        port (CLK                 : in std_logic;
+              RST                 : in std_logic;
+              CE                  : in std_logic;
+              ind_i               : in std_logic_vector (1 downto 0);
+              ind_j               : in std_logic_vector (1 downto 0);
+              cmp                 : in std_logic;
+              zero                : in std_logic;
+              btn                 : in std_logic;
+              init_btn            : out std_logic;
+              cpt_ram_grille_en   : out std_logic;
+              cpt_ram_grille_init : out std_logic;
+              addr_max_ram_grille : in std_logic;
+              cpt_ram_ext_en      : out std_logic;
+              cpt_ram_ext_init    : out std_logic;
+              addr_max_ram_ext    : in std_logic;
+              w_en_ram_grille     : out std_logic;
+              w_en_ram_ext        : out std_logic;
+              sel_addr            : out std_logic;
+              sel_addw            : out std_logic;
+              sel_dataw           : out std_logic_vector (1 downto 0));
     end component;
 
-    signal CLK          : std_logic;
-    signal RST          : std_logic;
-    signal CE           : std_logic;
-    signal btn_haut     : std_logic;
-    signal btn_bas      : std_logic;
-    signal btn_droit    : std_logic;
-    signal btn_gauche   : std_logic;
-    signal index_grille : std_logic_vector (3 downto 0);
-    signal cpt_init     : std_logic;
-    signal value        : std_logic_vector (255 downto 0);
+    signal CLK                 : std_logic;
+    signal RST                 : std_logic;
+    signal CE                  : std_logic;
+    signal ind_i               : std_logic_vector (1 downto 0);
+    signal ind_j               : std_logic_vector (1 downto 0);
+    signal cmp                 : std_logic;
+    signal zero                : std_logic;
+    signal btn                 : std_logic;
+    signal init_btn            : std_logic;
+    signal cpt_ram_grille_en   : std_logic;
+    signal cpt_ram_grille_init : std_logic;
+    signal addr_max_ram_grille : std_logic;
+    signal cpt_ram_ext_en      : std_logic;
+    signal cpt_ram_ext_init    : std_logic;
+    signal addr_max_ram_ext    : std_logic;
+    signal w_en_ram_grille     : std_logic;
+    signal w_en_ram_ext        : std_logic;
+    signal sel_addr            : std_logic;
+    signal sel_addw            : std_logic;
+    signal sel_dataw           : std_logic_vector (1 downto 0);
 
     constant TbPeriod : time := 1000 ns; -- EDIT Put right period here
     signal TbClock : std_logic := '0';
@@ -41,16 +61,26 @@ architecture tb of tb_machine_etats is
 begin
 
     dut : machine_etats
-    port map (CLK          => CLK,
-              RST          => RST,
-              CE           => CE,
-              btn_haut     => btn_haut,
-              btn_bas      => btn_bas,
-              btn_droit    => btn_droit,
-              btn_gauche   => btn_gauche,
-              index_grille => index_grille,
-              cpt_init     => cpt_init,
-              value        => value);
+    port map (CLK                 => CLK,
+              RST                 => RST,
+              CE                  => CE,
+              ind_i               => ind_i,
+              ind_j               => ind_j,
+              cmp                 => cmp,
+              zero                => zero,
+              btn                 => btn,
+              init_btn            => init_btn,
+              cpt_ram_grille_en   => cpt_ram_grille_en,
+              cpt_ram_grille_init => cpt_ram_grille_init,
+              addr_max_ram_grille => addr_max_ram_grille,
+              cpt_ram_ext_en      => cpt_ram_ext_en,
+              cpt_ram_ext_init    => cpt_ram_ext_init,
+              addr_max_ram_ext    => addr_max_ram_ext,
+              w_en_ram_grille     => w_en_ram_grille,
+              w_en_ram_ext        => w_en_ram_ext,
+              sel_addr            => sel_addr,
+              sel_addw            => sel_addw,
+              sel_dataw           => sel_dataw);
 
     -- Clock generation
     TbClock <= not TbClock after TbPeriod/2 when TbSimEnded /= '1' else '0';
@@ -61,26 +91,167 @@ begin
     stimuli : process
     begin
         -- EDIT Adapt initialization as needed
-        CE <= '0';
-        btn_haut <= '0';
-        btn_bas <= '0';
-        btn_droit <= '0';
-        btn_gauche <= '0';
-        index_grille <= (others => '0');
+        CE <= '1';
+        ind_i <= (others => '0');
+        ind_j <= (others => '0');
+        cmp <= '0';
+        zero <= '0';
+        btn <= '0';
+        addr_max_ram_grille <= '0';
+        addr_max_ram_ext <= '0';
 
         -- Reset generation
         -- EDIT: Check that RST is really your reset signal
-        RST <= '1';
-        wait for 100 ns;
         RST <= '0';
         wait for 100 ns;
+        RST <= '1';
+        wait for 100 ns;
 
-        -- EDIT Add stimuli here
+        
+        -- EDIT Add stimuli here --INIT
+        wait for 15 * TbPeriod;
+        addr_max_ram_grille <= '1';        
+                
+        -- EDIT Add stimuli here --RECHERCHE 0
         wait for 1 * TbPeriod;
-        btn_haut <= '1';
-        wait for 1 * TbPeriod;
-        btn_haut <= '0';
+        addr_max_ram_grille <= '0';
+        wait for 9 * TbPeriod;
+        zero <= '1';
+  
 
+        -- EDIT Add stimuli here --AJOUT NOMBRE
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        
+        --EXPORT GRILLE
+        wait for 15 * TbPeriod;
+        addr_max_ram_grille <= '1';        
+
+        
+        -- EDIT Add stimuli here --WAITING
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0'; 
+        wait for 4 * TbPeriod;
+        btn <= '1';       
+
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for 1 * TbPeriod;
+        btn <= '0';
+        wait for 4 * TbPeriod;
+        zero <= '1';
+        
+        -- EDIT Add stimuli here DECALAGE
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        wait for 4 * TbPeriod;
+        zero <= '1';
+        
+        -- EDIT Add stimuli here AJOUT 0
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for (10 * TbPeriod) + 300 ns;
+        addr_max_ram_grille <= '1';
+        
+        -- EDIT Add stimuli here Saving
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0';
+        
+        -- EDIT Add stimuli here CALCUL / SAVING
+        wait for 10 * TbPeriod;
+        cmp <= '1';
+        
+        -- EDIT Add stimuli here ADD
+        wait for 1 * TbPeriod;
+        cmp <= '0';
+        
+        -- EDIT Add stimuli here AJOUT ZERO
+        wait for 1 * TbPeriod;
+        cmp <= '0';
+        
+        -- EDIT Add stimuli here CALCUL / SAVING
+        wait for 5 * TbPeriod;
+        addr_max_ram_grille <= '1'; 
+        
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0'; 
+        wait for 10 * TbPeriod; -- DEc - ajout 0
+        zero <= '1';
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        wait for 5* TbPeriod;
+        addr_max_ram_grille <= '1'; 
+        
+        
+      --------------------------------------------------
+         
+        -- EDIT Add stimuli here --RECHERCHE 0
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0';
+        wait for 9 * TbPeriod;
+        zero <= '1';
+  
+
+        -- EDIT Add stimuli here --AJOUT NOMBRE
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        
+        --EXPORT GRILLE
+        wait for 15 * TbPeriod;
+        addr_max_ram_grille <= '1';        
+
+        
+        -- EDIT Add stimuli here --WAITING
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0'; 
+        wait for 4 * TbPeriod;
+        btn <= '1';       
+
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for 1 * TbPeriod;
+        btn <= '0';
+        wait for 4 * TbPeriod;
+        zero <= '1';
+        
+        -- EDIT Add stimuli here DECALAGE
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        wait for 4 * TbPeriod;
+        zero <= '1';
+        
+        -- EDIT Add stimuli here AJOUT 0
+        wait for 1 * TbPeriod;
+        zero <= '0';
+        
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for 10 * TbPeriod;
+        addr_max_ram_grille <= '1';
+        
+        -- EDIT Add stimuli here Saving
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0';
+        
+        -- EDIT Add stimuli here CALCUL / SAVING
+        wait for 10 * TbPeriod;
+        cmp <= '1';
+        
+        -- EDIT Add stimuli here AJOUT 0
+        wait for 1 * TbPeriod;
+        cmp <= '0';
+        
+        -- EDIT Add stimuli here CALCUL / SAVING
+        wait for 5 * TbPeriod;
+        addr_max_ram_grille <= '1'; 
+        
+        -- EDIT Add stimuli here RECHERCHE 0
+        wait for 1 * TbPeriod;
+        addr_max_ram_grille <= '0'; 
+        wait for 14 * TbPeriod;
+        addr_max_ram_grille <= '1'; 
+         
+         
         -- Stop the clock and hence terminate the simulation
         TbSimEnded <= '1';
         wait;
